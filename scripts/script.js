34a65d1cab0a5daf141e98b44e00f6b9e04645b8
Giulio <giulio.pietroiusti@gmail.com>
2017-03-13 14:34:13 +0100

    Replace list of entries correctly

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,31 @@ button.addEventListener("click", function(){
     getEntries();
 });
 
+function showResults(obj){
+    var entriesArray = obj.query.search; 
+    var oldEntriesDiv = document.getElementById("entries");
+    
+    var newEntriesDiv = document.createElement("div");
+    entriesArray.forEach(function(entry){
+        var el =  document.createElement("div");
+        el.innerHTML = entry.title;
+        newEntriesDiv.appendChild(el);
+    });
+
+    document.body.replaceChild(newEntriesDiv, oldEntriesDiv); 
+    newEntriesDiv.setAttribute("id", "entries");
+};
+
 function getEntries() {
     var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&origin=*&srsearch=" + input.value;
-    makeReq("GET", url,console.log);
+    makeReq("GET", url, showResults);
 
 }
 
 function makeReq(method, url, callback) {
     var req = new XMLHttpRequest();
     req.addEventListener("load", function(){
-        callback(req.responseText);
+        callback(JSON.parse(req.responseText));
     });
     req.open(method, url); 
     req.send();
