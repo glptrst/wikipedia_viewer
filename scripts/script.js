2be75dfb4bf6c99bca3830857c59b6ebd6d10c32
Giulio <giulio.pietroiusti@gmail.com>
2017-03-14 13:53:22 +0100

    Add links to entries

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
-var input = document.getElementById("entry");
-var button = document.getElementById("search");
-
-button.addEventListener("click", function(){
-    getEntries();
-});
+// search for entry when button is clicked
+(function addButtonListener(){
+    var button = document.getElementById("search");
+    button.addEventListener("click", function(){
+        getEntries();
+    });
+})();
 
 function showResults(obj){
     var entriesArray = obj.query.search; 
@@ -12,18 +13,19 @@ function showResults(obj){
     var newEntriesDiv = document.createElement("div");
     entriesArray.forEach(function(entry){
         var el =  document.createElement("div");
-        el.innerHTML = entry.title;
+        el.innerHTML = "<a href='http://en.wikipedia.org/wiki/"+entry.title+"'>" + entry.title + "</a>";
         newEntriesDiv.appendChild(el);
     });
 
-    document.body.replaceChild(newEntriesDiv, oldEntriesDiv); 
+    document.getElementById("main").replaceChild(newEntriesDiv, oldEntriesDiv);
+//    document.body.replaceChild(newEntriesDiv, oldEntriesDiv); 
     newEntriesDiv.setAttribute("id", "entries");
 };
 
 function getEntries() {
+    var input = document.getElementById("entry");
     var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&origin=*&srsearch=" + input.value;
     makeReq("GET", url, showResults);
-
 }
 
 function makeReq(method, url, callback) {
